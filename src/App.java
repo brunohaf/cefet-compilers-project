1832d3017c8068a94b8f9ba4e2cf5029961c9bd2
brunohaf <bruno3145@gmail.com>
2021-04-07 01:10:49 -0300

    refacts: testCode path to be dinamically asserted

--- src/App.java
+++ src/App.java
@@ -7,7 +7,7 @@ import SyntacticAnalyzer.SyntaticAnalyzer;
 public class App {
     public static void main(String[] args) throws Exception {
         try {
-            Lexer lexer = new Lexer("/home/heitor/projects/tmp/cefet-compilers-project/src/testCode");
+            Lexer lexer = new Lexer("../cefet-compilers-project/src/testCode");
             ArrayList tokenList = lexer.getTokenList();
 
             SyntaticAnalyzer syntaticAnalyzer = new SyntaticAnalyzer(tokenList);
